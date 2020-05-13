9559539e585b5008fb0f52cc507e2a6ec76904f9
Will Johnson <longklaw@gmail.com>
2020-05-13 14:01:07 -0700

    No longer querying data on app load

--- app/components/common/http.js
+++ app/components/common/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 function httpCall(url, method, params) {
     return axios({
         method: method,
-        url: window.location.protocol + '//' + window.location.hostname + url,
+        url: window.location.protocol + '//' + window.location.hostname + (window.location.port !== "" ? (":" + window.location.port) : "") + url,
         params: params,
       }).then(res => 
         {
